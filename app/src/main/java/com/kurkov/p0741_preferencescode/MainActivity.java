80185d7777cf371250ab4a9c6f057a394e0abc04
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2015-09-20 21:14:15 +0300

    Initial commit

---
+++ app/src/main/java/com/kurkov/p0741_preferencescode/MainActivity.java
@@ -0,0 +1,22 @@
+package com.kurkov.p0741_preferencescode;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem mi = menu.add(0, 1, 0, "Preferences");
+        mi.setIntent(new Intent(this, PrefActivity.class));
+        return super.onCreateOptionsMenu(menu);
+    }
+}
